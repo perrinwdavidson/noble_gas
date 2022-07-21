02a5aa6666147c08c3bba856d3f30c36febc3738
Perrin Davidson <perrinwdavidson@gmail.com>
2022-07-21 01:15:31 -0400

    starting plotting routines -- done before debug

--- cmip5/src/matlab/plot_ice.m
+++ cmip5/src/matlab/plot_ice.m
@@ -5,17 +5,37 @@ function plot_ice(filename1, products)
 %   contact: perrinwdavidson@gmail.com
 %   date: 06.07.22
 %--------------------------------------------------------------------------
-%%  let me know what is going on
+%%  configure 
+%   add paths ::
+addpath('/Users/perrindavidson/Documents/MATLAB/toolboxes/plotting/m_map');
+
+%   let me know what is going on ::
 disp(append('Plotting ', upper(variable))); 
 
-%%  make month array ::
+%   make month array ::
 month_names = {'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'};
+
+%   get variables names ::
+group_names = ncread(filename1, 'group_names');
+variable_names = ncread(filename1, 'variable_names');
+
+%   get number of models ::
+NUMMOD = size(group_names, 1);
+
+%   set number of months ::
+NUMMON = 12;
+
+%   set colors ::
+color_limits = 0 : 0.1 : 1; 
+color_map = winter(10); 
 
 %%  loop through all models
 for iMod = 1 : 1 : NUMMOD
 
     %%  load data ::
-    
+    model_data = ncread(filename1, append(group_names{iMod}, variable_names{iMod}));
+    model_lon = ncread(filename1, append(group_names{iMod}, 'lon'));
+    model_lat = ncread(filename1, append(group_names{iMod}, 'lat'));
 
     %%  plot ::
     %   start plot ::        
@@ -25,10 +45,13 @@ for iMod = 1 : 1 : NUMMOD
     %   loop through all months ::
     for iMon = 1 : 1 : NUMMON
 
-        %-  plot contourf ::
+        %   get monthly data ::
+        model_data_month = model_data(:, :, iMon);
+
+        %   plot contourf ::
         nexttile();
         m_proj('miller', 'lon', [0 360], 'lat', [min(y) max(y)]);
-        m_contourf(lon, lat, shaped_interp, color_limits, 'edgecolor', 'none');
+        m_contourf(model_lon, model_lat, model_data_month, color_limits, 'edgecolor', 'none');
         m_coast('patch', [.8 .8 .8]);
         m_grid('box', 'fancy', 'tickdir', 'in');
         colormap(color_map);
@@ -42,7 +65,7 @@ for iMod = 1 : 1 : NUMMOD
 
     %   set and save plot ::
     set(gcf, 'position', [0, 0, 1920, 1000]); 
-    exportgraphics(t, fullfile('plots', age, strcat(variable, '_', age, '_', products{iMod, 1}, '.png')), 'resolution', 300); 
+    exportgraphics(t, fullfile('plots', 'ice', age, strcat(variable, '_', age, '_', products{iMod, 1}, '.png')), 'resolution', 300); 
 
 end
 
--- cmip5/src/matlab/plot_mean_wind.m
+++ cmip5/src/matlab/plot_mean_wind.m
@@ -5,40 +5,65 @@ function plot_mean_wind(variable, filename1, filename2)
 %   contact: perrinwdavidson@gmail.com
 %   date: 06.07.22
 %--------------------------------------------------------------------------
-%%  let me know what is going on
+%%  configure
+%   let me know what is going on ::
 disp(append('Plotting zonally averaged ', upper(variable)));
 
-%%  make month array ::
+%   make month array ::
 month_names = {'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'};
+
+%   get variables names ::
+group_names = ncread(filename, 'group_names');
+variable_names = ncread(filename, 'variable_names');
+
+%   get number of models ::
+NUMMOD = size(group_names, 1);
+
+%   set number of months ::
+NUMMON = 12;
 
 %%  loop through all models
 for iMod = 1 : 1 : NUMMOD
 
     %%  load data ::
+    %   file 1 ::
+    model_data_1 = ncread(filename1, append(group_names{iMod}, variable_names{iMod}));
+    model_lat_1 = ncread(filename1, append(group_names{iMod}, 'lat'));
 
+    %   file 2 ::
+    model_data_2 = ncread(filename2, append(group_names{iMod}, variable_names{iMod}));
+    model_lat_2 = ncread(filename2, append(group_names{iMod}, 'lat'));
 
-    %%  plot ::    
-    %   start plot ::   
+    %%  plot ::
+    %   start plot ::        
     close;
     t = tiledlayout(3, 4, 'tileSpacing', 'compact');
 
     %   loop through all months ::
     for iMon = 1 : 1 : NUMMON
 
-        %-  plot ::
-        nexttile();
-        
+        %   get monthly data ::
+        model_data_month = model_data(:, iMon);
 
+        %   plot contourf ::
+        nexttile();
+        hold('on');
+        plot(model_data_1, model_lat_1, '-k', 'lineWidth', 1.5);
+        plot(model_data_2, model_lat_2, '--k', 'lineWidth', 1.5);
+        hold('off');
+        legend('LGM', 'PIC')
+        xlabel('Wind Speed (m s^{-1})')
+        ylabel('Latitude')
         title(month_names(iMon));
 
     end
 
     %   title ::
-    title(t, append(upper(age), ' ', products{iMod, 1}, ' Sea Ice Fraction'), 'fontWeight', 'bold')
+    title(t, append('Zonally Averaged ', products{iMod, 1}, ' ', upper(variable)), 'fontWeight', 'bold')
 
     %   set and save plot ::
     set(gcf, 'position', [0, 0, 1920, 1000]); 
-    exportgraphics(t, fullfile('plots', age, strcat(variable, '_', age, '_', products{iMod, 1}, '.png')), 'resolution', 300); 
+    exportgraphics(t, fullfile('plots', 'wind', strcat(variable, '_', age, '_', products{iMod, 1}, '.png')), 'resolution', 300); 
 
 end
 
